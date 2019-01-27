02c35e0a7518c6fa47ed1250402a5cae2942e1bb
Brian Bycynski <high5bri@hotmail.com>
2019-01-27 07:01:28 -0500

    finished lecture 22

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import { HomeHeader } from '../utils'
+import img from '../images/bcg/homeBcg.jpeg'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>This is Home Page</h3>
+    <HomeHeader img={img}>Hello from home header</HomeHeader>
   </Layout>
 )
 
