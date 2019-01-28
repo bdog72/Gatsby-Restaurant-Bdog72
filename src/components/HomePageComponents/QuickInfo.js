e47eb3a75614a528d3c5e4a80c6dc28fd65dda6a
Brian Bycynski <high5bri@hotmail.com>
2019-01-28 10:45:28 -0500

    finished lecture 31

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 import { Section, Title, SectionButton } from '../../utils'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { styles } from '../../utils'
 
 export default class QuickInfo extends Component {
   render() {
     return (
       <Section>
         <Title message="let us tell you" title="our mission" />
-        <SectionButton>about</SectionButton>
+        <QuickInfoWrapper>
+          <p className="text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+            porro sit exercitationem dolorum veritatis ad facere saepe cum
+            tempore atque in, laudantium, soluta temporibus ut voluptatibus
+            numquam doloribus, quo nisi?
+          </p>
+          <Link to="/about/" style={{ textDecoration: 'none' }}>
+            <SectionButton style={{ margin: '2rem auto' }}>about</SectionButton>
+          </Link>
+        </QuickInfoWrapper>
       </Section>
     )
   }
 }
+
+const QuickInfoWrapper = styled.div`
+  width: 90vw;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`
