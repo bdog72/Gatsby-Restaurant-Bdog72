166a202d397ea356059d0f69dce7bc2a6a622d28
Brian Bycynski <high5bri@hotmail.com>
2019-01-28 04:52:35 -0500

    finished lecture 26

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -5,7 +5,7 @@ export const colors = {
   mainYellow2: `#F2AF29`,
   mainGrey: `#474747`,
 }
-export const textSlanted = `font-family:'Caveat', cursive;`
+export const textSlanted = `font-family:'Caveat Brush', cursive;`
 
 export const transDefault = 'transition:all 0.5s ease-in-out'
 export const transFunction = (
